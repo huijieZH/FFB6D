d208d1b87531d48a37be092713b2187990292296
370620267@qq.com <huijiezh@umich.edu>
2021-11-22 20:16:12 -0500

    update

--- ffb6d/progresslabeler_test.sh
+++ ffb6d/progresslabeler_test.sh
@@ -1,4 +1,4 @@
-model_sensor=L515
+model_sensor=D435
 dataset_sensor=L515
 checkpoint=train_log/ycb/checkpoints/FFB6D_fine_tune_$model_sensor.pth.tar
 # checkpoint=train_log/ycb/checkpoints/FFB6D_baseline.pth.tar
@@ -6,7 +6,7 @@ checkpoint=train_log/ycb/checkpoints/FFB6D_fine_tune_$model_sensor.pth.tar
 echo "model_sensor: " $model_sensor ", dataset_sensor: " $dataset_sensor
 
 # --test --sensor_name --template --ratio
-python datasets/ycb/dataset_config/generate_list.py test $dataset_sensor train_scene[0-8] 0.1
+python datasets/ycb/dataset_config/generate_list.py test $dataset_sensor test_scene1 1
 
 python -m torch.distributed.launch --nproc_per_node=1 train_ycb.py --gpu 0 -eval_net -checkpoint $checkpoint -test -test_pose
 # echo $tst_mdl 
