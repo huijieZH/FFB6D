62f754fab02b4eab684007a587e7cdaad0919508
370620267@qq.com <huijiezh@umich.edu>
2022-02-20 21:17:27 -0500

    finish progress labeller

--- ffb6d/progresslabeler_test.sh
+++ ffb6d/progresslabeler_test.sh
@@ -1,12 +1,40 @@
-model_sensor=D435
-dataset_sensor=L515
-checkpoint=train_log/ycb/checkpoints/FFB6D_fine_tune_$model_sensor.pth.tar
-# checkpoint=train_log/ycb/checkpoints/FFB6D_baseline.pth.tar
+# model_sensor=primesense
+# dataset_sensor=primesense
 
-echo "model_sensor: " $model_sensor ", dataset_sensor: " $dataset_sensor
+# checkpoint=train_log/ycb/checkpoints/FFB6D_$model_sensor.pth.tar
+# echo "model_sensor: " $model_sensor ", dataset_sensor: " $dataset_sensor
 
-# --test --sensor_name --template --ratio
-python datasets/ycb/dataset_config/generate_list.py test $dataset_sensor test_scene1 1
+# # --test --sensor_name --template --ratio
+# python /home/huijie/research/progresslabeller/FFB6D/ffb6d/datasets/ycb/dataset_config/generate_list.py test $dataset_sensor test_scene* 0.3
 
-python -m torch.distributed.launch --nproc_per_node=1 train_ycb.py --gpu 0 -eval_net -checkpoint $checkpoint -test -test_pose
-# echo $tst_mdl 
+# python -m torch.distributed.launch --nproc_per_node=1 train_ycb.py --gpu 0 -eval_net -checkpoint $checkpoint -test -test_pose
+
+# for model_sensor in primesense D435 L515; do
+# for dataset_sensor in primesense D435 L515; do
+#     if [ "$dataset_sensor" != "primesense" ] || [ "$model_sensor" != "primesense" ]; then
+#         checkpoint=train_log/ycb/checkpoints/FFB6D_$model_sensor.pth.tar
+#         echo "model_sensor: " $model_sensor ", dataset_sensor: " $dataset_sensor
+
+#         # --test --sensor_name --template --ratio
+#         python /home/huijie/research/progresslabeller/FFB6D/ffb6d/datasets/ycb/dataset_config/generate_list.py test $dataset_sensor test_scene* 0.3
+
+#         python -m torch.distributed.launch --nproc_per_node=1 train_ycb.py --gpu 0 -eval_net -checkpoint $checkpoint -test -test_pose
+#     fi
+# done
+# done
+
+for model_sensor in baseline; do
+for dataset_sensor in primesense D435 L515; do
+    if [ "$dataset_sensor" != "primesense" ] || [ "$model_sensor" != "primesense" ]; then
+        checkpoint=train_log/ycb/checkpoints/FFB6D_$model_sensor.pth.tar
+        echo "model_sensor: " $model_sensor ", dataset_sensor: " $dataset_sensor
+
+        # --test --sensor_name --template --ratio
+        python /home/huijie/research/progresslabeller/FFB6D/ffb6d/datasets/ycb/dataset_config/generate_list.py test $dataset_sensor test_scene* 0.3
+
+        python -m torch.distributed.launch --nproc_per_node=1 train_ycb.py --gpu 0 -eval_net -checkpoint $checkpoint -test -test_pose
+    fi
+done
+done
+
+
